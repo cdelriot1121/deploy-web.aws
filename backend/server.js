87afc361411ff6c7943fee3bfb8c78c426f3e6dc
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-06-16 19:20:14 -0500

    config & feat: integrar dependencias para archivos .env y tambien cambios en la configuracion de al app

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors'); 
 
 const app = express();
-const port = 3000;
 
 app.use(cors()); 
 app.use(express.json()); 
 
 const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root', 
-    password: process.env.DB_PASSWORD || '###',
-    database: process.env.DB_NAME || 'task_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
+const port = process.env.PORT || 3000;
 
 async function getConnection() {
     try {
